bf57a9bef92b4c67f21238b5154eb3e0ecc0a75b
Renan <renandf@gmail.com>
2016-01-08 21:25:25 +1100

    update all views with categories; fixed space between title and content

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 9)
+    @articles = Article.paginate(page: params[:page], per_page: 9).order('created_at DESC')
   end
 
   def new
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
 
     def require_same_user
